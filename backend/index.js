bd2f0a20b55cb8bbc48e8d7ff58f774540881460
Laura Gandiglio <128721674+LauraGandiglio@users.noreply.github.com>
2025-06-18 19:21:39 -0300

    Implementando endpoint GET
    Definición de los endpoints GET /api/tasks para obtener todas las tareas, y GET /api/tasks/:id para tareas particulares. Introducción del manejo de error en caso de solicitar una tarea de id inexistente.

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,40 @@
 const express = require("express");
 const app = express();
 
+let tasks = [
+  {
+    id: 0,
+    title: "Barrer",
+    description: "Barrer el comedor",
+    completed: false,
+    createdAt: "Poner Fecha",
+  },
+  {
+    id: 1,
+    title: "Doblar ropa",
+    description: "Doblar la ropa de la silla",
+    completed: true,
+    createdAt: "Poner Fecha",
+  },
+];
+
+// GET
+app.get("/", (req, res) => {
+  res.send("<h1>Lista de tareas</h1>");
+});
+app.get("/api/tasks", (req, res) => {
+  res.json(tasks);
+});
+app.get("/api/tasks/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const task = tasks.find((task) => task.id === id);
+  if (task) {
+    res.json(task);
+  } else {
+    res.status(404).send("Lo siento, la tarea no existe.");
+  }
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
