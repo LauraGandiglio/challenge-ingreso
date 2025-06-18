7f8a6a9c28e5c6894520490086e6614f1cf27145
Laura Gandiglio <128721674+LauraGandiglio@users.noreply.github.com>
2025-06-18 20:00:15 -0300

    Implementando endpoint DELETE
    Definición del endpoint DELETE /api/tasks/:id para eliminar una tarea particular. Corrección donde se estipulaba que el id de la tarea debía ser del tipo number, ahora debe ser un string.

--- backend/index.js
+++ backend/index.js
@@ -3,14 +3,14 @@ const app = express();
 
 let tasks = [
   {
-    id: 0,
+    id: "0",
     title: "Barrer",
     description: "Barrer el comedor",
     completed: false,
     createdAt: "Poner Fecha",
   },
   {
-    id: 1,
+    id: "1",
     title: "Doblar ropa",
     description: "Doblar la ropa de la silla",
     completed: true,
@@ -26,13 +26,21 @@ app.get("/api/tasks", (req, res) => {
   res.json(tasks);
 });
 app.get("/api/tasks/:id", (req, res) => {
-  const id = Number(req.params.id);
+  const id = req.params.id;
   const task = tasks.find((task) => task.id === id);
   if (task) {
     res.json(task);
   } else {
     res.status(404).send("Lo siento, la tarea no existe.");
   }
+});
+
+// DELETE
+app.delete("/api/tasks/:id", (req, res) => {
+  console.log(req.params.id);
+  const id = req.params.id;
+  tasks = tasks.filter((task) => task.id !== id);
+  res.status(204).end();
 });
 
 const PORT = 3001;
