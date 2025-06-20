b6f288f0f0c573e989ae008c25e62b4482e5d673
Laura Gandiglio <128721674+LauraGandiglio@users.noreply.github.com>
2025-06-20 17:11:26 -0300

    Implementando edición de la tarea y estado
    Definición de los botones para editar el titulo y/o descripción de la tarea. Definición del botón para marcar la tarea completa/incompleta. Configuración de los manejadores de eventos correspondientes a cada botón. Configuración de un pequeño formulario para editar la tarea con renderizado condicional.

--- frontend/src/components/TaskItem.jsx
+++ frontend/src/components/TaskItem.jsx
@@ -1,4 +1,10 @@
+import { useState } from "react";
+
 const TaskItem = ({ task, setTaskList }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedTitle, setEditedTitle] = useState(task.title);
+  const [editedDescription, setEditedDescription] = useState(task.description);
+
   //Maneja el evento del click al botón de eliminar tarea
   const handleDelete = (id) => {
     fetch(`http://localhost:3001/api/tasks/${id}`, {
@@ -11,13 +17,82 @@ const TaskItem = ({ task, setTaskList }) => {
       .catch((error) => console.error("Error al eliminar tarea:", error));
   };
 
+  //Editar tarea
+  const handleEdit = () => {
+    const updatedTask = {
+      ...task,
+      title: editedTitle,
+      description: editedDescription,
+    };
+
+    fetch(`http://localhost:3001/api/tasks/${task.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updatedTask),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setTaskList((prevTasks) =>
+          prevTasks.map((task) => (task.id === data.id ? data : task))
+        );
+        setIsEditing(false);
+      })
+      .catch((error) => console.error("Error al editar:", error));
+  };
+
+  //Cambiar estado de tarea
+  const toggleState = () => {
+    const updatedTask = {
+      ...task,
+      completed: !task.completed,
+    };
+    fetch(`http://localhost:3001/api/tasks/${task.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updatedTask),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setTaskList((prevTasks) =>
+          prevTasks.map((task) => (task.id === data.id ? data : task))
+        );
+      })
+      .catch((error) => console.error("Error al cambiar completado:", error));
+  };
+
   return (
-    <div>
-      <h3>{task.title}</h3>
-      <p>{task.description}</p>
-      {task.completed ? <p>Tarea completada</p> : <p>Tarea pendiente</p>}
-      <button onClick={() => handleDelete(task.id)}>Eliminar</button>
-    </div>
+    <>
+      {isEditing ? (
+        <div>
+          <input
+            type="text"
+            value={editedTitle}
+            onChange={(e) => setEditedTitle(e.target.value)}
+          />
+          <input
+            type="text"
+            value={editedDescription}
+            onChange={(e) => setEditedDescription(e.target.value)}
+          />
+          <button onClick={handleEdit}>Guardar</button>
+        </div>
+      ) : (
+        <div>
+          <h3>{task.title}</h3>
+          <p>{task.description}</p>
+          {task.completed ? <p>Tarea completada</p> : <p>Tarea pendiente</p>}
+          <button onClick={() => handleDelete(task.id)}>Eliminar</button>
+          <button onClick={() => setIsEditing(!isEditing)}>Editar</button>
+          <button onClick={toggleState}>
+            {task.completed ? "Marcar incompleta" : "Marcar completa"}
+          </button>
+        </div>
+      )}
+    </>
   );
 };
 
